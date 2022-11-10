e5b8f814101899f781c0d031460ce54c5e0d3286
maro5397 <lsj000424@gmail.com>
2022-11-10 12:15:54 -0500

    ✨ feat: add tcpclient codes

---
+++ tcpclient/main.cpp
@@ -0,0 +1,11 @@
+#include "tcpclient.h"
+
+int main()
+{
+    char buf[BUFSIZE];
+    TcpClient client;
+    client.connect("192.168.118.133", 9090);
+    client.send("Test data to server", 500);
+    client.recv(buf, BUFSIZE);
+    DLOG(INFO) << buf;
+}
