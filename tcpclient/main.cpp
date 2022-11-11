c280b10bf560fb23231401a1ff1ffffd4df3f8c7
maro5397 <lsj000424@gmail.com>
2022-11-11 10:02:34 -0500

    ✨ feat(add): timer and file send after read and recv before save

--- tcpclient/main.cpp
+++ tcpclient/main.cpp
@@ -1,11 +1,99 @@
 #include "tcpclient.h"
+#include <sys/stat.h>
+#include <ctime>
+#include <fstream>
 
-int main()
+float getConnTime(TcpClient* client, std::string ip, int port);
+float getSendTime(TcpClient* client, std::string filename);
+void setDirAndLog();
+void usage();
+
+int main(int argc, char* argv[])
 {
-    char buf[BUFSIZE];
-    TcpClient client;
-    client.connect("192.168.118.133", 9090);
-    client.send("Test data to server", 500);
-    client.recv(buf, BUFSIZE);
-    DLOG(INFO) << buf;
+    setDirAndLog();
+    if(argc < 4) {
+        DLOG(INFO) << "usage incorrect";
+        usage();
+        exit(-1);
+    }
+
+    std::string ip(argv[1]);
+    int port = atoi(argv[2]);
+    std::string filename(argv[3]);
+    
+    DLOG(INFO) << "usage correct parse complete";
+    DLOG(INFO) << "ip: " << ip;
+    DLOG(INFO) << "port: " << port;
+    DLOG(INFO) << "filename: " << filename;
+
+    TcpClient* client = new TcpClient();
+    getConnTime(client, ip, port);
+    getSendTime(client, filename);
+}
+
+float getSendTime(TcpClient* client, std::string filename)
+{
+    clock_t start, finish;
+    float spent;
+    char sendbuf[BUFSIZE];
+    char recvbuf[BUFSIZE];
+    std::ifstream fin("files/" + filename);
+    int size = 0;
+    
+    if(fin.is_open()) {
+        fin.seekg(0, std::ios::end);
+        size = fin.tellg();
+        fin.seekg(0, std::ios::beg);
+        fin.read(sendbuf, size);
+    }
+    fin.close();
+    
+    DLOG(INFO) << "data size(byte): " << size << "byte(s)";
+
+    start = clock();
+    client->send(sendbuf, BUFSIZE);
+    client->recv(recvbuf, BUFSIZE);
+    finish = *((long*)recvbuf);
+
+    spent = (float)(finish - start);
+    DLOG(INFO) << "server finish clock: " << finish;
+    DLOG(INFO) << "data transmission time(s): " << spent / CLOCKS_PER_SEC << "s";
+    return spent;
+}
+
+float getConnTime(TcpClient* client, std::string ip, int port)
+{
+    clock_t start, finish;
+    float spent;
+
+    start = clock();
+    client->connect(ip, port);
+    finish = clock();
+
+    spent = (float)(finish - start);
+    DLOG(INFO) << "create connection time(s): " << spent / CLOCKS_PER_SEC << "s";
+    return spent;
+}
+
+void setDirAndLog()
+{
+    if (mkdir("logs", 0777) == -1)
+        DLOG(INFO) << "logs directory create Error : " << strerror(errno);
+    else
+        DLOG(INFO) << "logs directory create Success";
+
+    if (mkdir("files", 0777) == -1)
+        DLOG(INFO) << "files directory create Error : " << strerror(errno);
+    else
+        DLOG(INFO) << "files directory create Success";
+    
+    FLAGS_alsologtostderr = 1;
+    google::SetLogDestination(google::INFO, "logs/");
+    //google::InitGoogleLogging("logs/");
+}
+
+void usage()
+{
+	DLOG(INFO) << "syntax : main <server-ip> <server-port> <filename>";
+	DLOG(INFO) << "sample : main 192.168.1.2 8080 testdata.txt";
 }
--- tcpserver/main.cpp
+++ tcpserver/main.cpp
@@ -1,42 +1,89 @@
 #include "tcpserver.h"
+#include <sys/stat.h>
+#include <ctime>
+#include <fstream>
 
-class EchoTcp : public TcpServer
+class TimerTcp : public TcpServer
 {
 public:
-    EchoTcp();
-    ~EchoTcp() {}
+    TimerTcp();
+    ~TimerTcp() {}
 
 protected:
     void handleClnt(TcpClientSocket* clntsock) override;
+    void saveFile(char* recvbuf);
 };
 
-EchoTcp::EchoTcp() {
+TimerTcp::TimerTcp() {
 }
 
-void EchoTcp::handleClnt(TcpClientSocket* clntsock) {
-    char buffer[BUFSIZE];
-    char message[] = "HI, this is server";
+void TimerTcp::handleClnt(TcpClientSocket* clntsock) {
+    char recvbuf[BUFSIZE];
     int len = 0;
     int count = 0;
-    while((len = clntsock->recv(buffer, BUFSIZE)) != -1) {
+    clock_t finish = 0;
+    while((len = clntsock->recv(recvbuf, BUFSIZE)) != -1) {
         if(len == 0) {
             DLOG(INFO) << "clntsock is shutdown";
             return;
         }
-        DLOG(INFO) << count++;
-        DLOG(INFO) << "=====recv data from client=====";
-        DLOG(INFO) << buffer;
-        DLOG(INFO) << "=====recv data from client=====";
-        clntsock->send(message, strlen(message) + 1);
+        finish = clock();
+        char* sendbuf = (char*)&finish;
+        clntsock->send(sendbuf, strlen(sendbuf));
+        DLOG(INFO) << "recv data number: " << count++;
+        DLOG(INFO) << "\n===============recv data from client===============\n" 
+                   << recvbuf 
+                   << "\n===============recv data from client===============\n";
+        DLOG(INFO) << "finish clock: " << finish;
+        //saveFile(recvbuf);
     }
     return;
 }
 
-int main()
+void TimerTcp::saveFile(char* recvbuf)
 {
-    EchoTcp server;
-    server.start(9090);
-    sleep(60);
-    server.stop();
+    time_t rawtime;
+    time(&rawtime);
+    struct tm* timeinfo = localtime(&rawtime);
+    char buffer[80];
+    strftime(buffer, sizeof(buffer), "%d-%m-%Y %H:%M:%S", timeinfo);
+    std::string datetime(buffer);
+
+    std::ofstream fout("files/" + datetime + ".txt");
+    if(fout.is_open()) {
+        fout.write(recvbuf, strlen(recvbuf));
+    }
+    fout.close();
+    return;
+}
+
+void usage()
+{
+	DLOG(INFO) << "syntax : main <server-port>";
+	DLOG(INFO) << "sample : main 8080";
+}
+
+int main(int argc, char* argv[])
+{
+    if (mkdir("files", 0777) == -1)
+        DLOG(INFO) << "files directory create Error : " << strerror(errno);
+    else
+        DLOG(INFO) << "files directory create Success";
+    
+    if(argc < 2) {
+        DLOG(INFO) << "usage incorrect";
+        usage();
+        exit(-1);
+    }
+
+    int port = atoi(argv[1]);
+    
+    DLOG(INFO) << "usage correct parse complete";
+    DLOG(INFO) << "port: " << port;
+
+    TimerTcp* server = new TimerTcp();
+    server->start(port);
+    sleep(6000);
+    server->stop();
     return 0;
 }
